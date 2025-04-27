24b11c2b4f85d736f9a8d7610029164c123ccbc2
Manjeet Yadav <162901609+Manjeet-Yadav-777@users.noreply.github.com>
2025-04-27 17:38:58 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: process.env.FRONTEND_URL,
+    origin: "https://blinkit-frontend-dynz.onrender.com",
   })
 );
 app.use(express.json());
